61b17955c4196dd30eb3c3a38abf3d9c803248e4
Roei Ben Zion <roei.benzion@gmail.com>
2024-11-06 16:42:16 +0200

    COLAB(!) pretrain script

--- LLaVA/scripts/v1_5/pretrain.sh
+++ LLaVA/scripts/v1_5/pretrain.sh
@@ -15,12 +15,12 @@ deepspeed llava/train/train_mem.py \
     --bf16 False \
     --output_dir ./checkpoints/llava-v1.5-1b-pretrain \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 24000 \
+    --save_steps 1000 \
     --save_total_limit 1 \
     --learning_rate 1e-3 \
     --weight_decay 0. \
