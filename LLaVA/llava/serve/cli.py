bc8e4f017464c1edddb02a367ede4fef5d307d6f
Roei Ben Zion <roei.benzion@gmail.com>
2024-11-08 11:21:13 +0200

    cli fits for automatic inference during train

--- LLaVA/llava/serve/cli.py
+++ LLaVA/llava/serve/cli.py
@@ -63,7 +63,7 @@ def main(args):
         image_tensor = [image.to(model.device, dtype=torch.float16) for image in image_tensor]
     else:
         image_tensor = image_tensor.to(model.device, dtype=torch.float16)
-
+    '''
     while True:
         try:
             inp = input(f"{roles[0]}: ")
@@ -108,7 +108,49 @@ def main(args):
 
         if args.debug:
             print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
+        '''
+    try:
+        inp = f"{roles[0]}: What's in the picutre?"
+    except EOFError:
+        inp = ""
+    if not inp:
+        print("exit...")
 
+    print(f"{roles[1]}: ", end="")
+
+    if image is not None:
+        # first message
+        if model.config.mm_use_im_start_end:
+            inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+        else:
+            inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
+        image = None
+    
+    conv.append_message(conv.roles[0], inp)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
+
+    input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
+    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+    keywords = [stop_str]
+    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+    with torch.inference_mode():
+        output_ids = model.generate(
+            input_ids,
+            images=image_tensor,
+            image_sizes=[image_size],
+            do_sample=True if args.temperature > 0 else False,
+            temperature=args.temperature,
+            max_new_tokens=args.max_new_tokens,
+            streamer=streamer,
+            use_cache=True)
+
+    outputs = tokenizer.decode(output_ids[0]).strip()
+    conv.messages[-1][-1] = outputs
+
+    if args.debug:
+        print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
